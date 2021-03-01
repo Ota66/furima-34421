7eb9c9b9e292a2d844b65a7a21f6565583393462
Ota66 <mitu.xxo66@gmail.com>
2021-03-01 20:49:11 +0900

    itemsコントローラーbefore_actionでまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_redirect, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(private_params)
       redirect_to item_path
     else
@@ -40,6 +38,14 @@ class ItemsController < ApplicationController
 
   def private_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_redirect
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
 end
